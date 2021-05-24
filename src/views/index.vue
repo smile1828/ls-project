2f5911e04fc9e4e3efc528f4dce3b6ed4d45bb8e
liulishuang <794844674@qq.com>
2021-05-24 18:29:15 +0800

    框架基础

---
+++ src/views/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <el-container style="width: 100%;height: 100%">
+    <el-header height="50px">
+    </el-header>
+    <el-container direction="horizontal">
+      <el-aside ></el-aside>
+      <el-main></el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+export default {
+  name: 'index'
+}
+</script>
+
+<style scoped>
+
+</style>
